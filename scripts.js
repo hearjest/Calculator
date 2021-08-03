386f77709509666a26dce8003690e28c0d4cf724
Brian <bzengprsnl@gmail.com>
2021-08-03 17:14:38 -0400

    Found a way to seperate the screen and expression

--- scripts.js
+++ scripts.js
@@ -1,7 +1,25 @@
-const buttons=document.querySelectorAll('.calcButton');
+const buttons=document.querySelectorAll('.button');
 const screen=document.querySelector('#screen');
+const equals=document.getElementById('equals').addEventListener('click',function(){
+    calculate();
+});
+let expression=[];
 buttons.forEach((button)=>{
     button.addEventListener('click',function(){
         screen.textContent+=button.textContent;
+        if(button.textContent.charCodeAt(0)>=48&&button.textContent.charCodeAt(0)<=57){
+            expression+=Number(button.textContent);
+        }else{
+            expression+=` ${button.textContent} `;
+        }
     })
-})
+});
+
+function calculate(){//An array length of 0 or an even array length means
+    console.log(expression.split(' '));
+}
+
+function clear(){
+    screen.textContent='';
+    expression=[];
+}
