f9326f2b0611f2d7271a559c959026e9641d0c2a
Brian <bzengprsnl@gmail.com>
2021-08-04 12:54:44 -0400

    Calcuator functions

--- scripts.js
+++ scripts.js
@@ -3,7 +3,9 @@ const screen=document.querySelector('#screen');
 const equals=document.getElementById('equals').addEventListener('click',function(){
     calculate();
 });
-let expression=[];
+let expression='';
+let equation;
+let result;
 buttons.forEach((button)=>{
     button.addEventListener('click',function(){
         screen.textContent+=button.textContent;
@@ -15,11 +17,26 @@ buttons.forEach((button)=>{
     })
 });
 
-function calculate(){//An array length of 0 or an even array length means
-    console.log(expression.split(' '));
+function calculate(){//An array length of 0 or an even array length are invalid
+    equation=expression.split(' ');
+    result=Number(equation[0]);
+    for(let i=1;i<equation.length;i+=2){
+        if(equation[i]==='÷'){
+            result/=Number(equation[i+1]);
+        }else if(equation[i]==='x'){
+            result*=Number(equation[i+1]);
+        }else if(equation[i]==='-'){
+            result-=Number(equation[i+1]);
+        }else if(equation[i]==='+'){
+            result+=Number(equation[i+1]);
+        }
+    }
+    clear();
+    screen.textContent=result;
 }
 
 function clear(){
     screen.textContent='';
-    expression=[];
+    expression='';
+    equation=[];
 }
