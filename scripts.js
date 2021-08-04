8481e48e53d0db53882c13babce347fa82fe3b0a
Brian <bzengprsnl@gmail.com>
2021-08-04 13:33:26 -0400

    Added clear button functionality

--- scripts.js
+++ scripts.js
@@ -3,16 +3,24 @@ const screen=document.querySelector('#screen');
 const equals=document.getElementById('equals').addEventListener('click',function(){
     calculate();
 });
+document.getElementById('clear').addEventListener('click',function(){
+    clear();
+});
+document.getElementById('delete').addEventListener('click',function(){
+    deleteThing();
+});
+
 let expression='';
 let equation;
 let result;
+
 buttons.forEach((button)=>{
     button.addEventListener('click',function(){
-        if(button.textContent.charCodeAt(0)>=48&&button.textContent.charCodeAt(0)<=57){
+        if(button.textContent.charCodeAt(0)>=48&&button.textContent.charCodeAt(0)<=57){//Is a number
             expression+=Number(button.textContent);
             screen.textContent+=button.textContent;
         }else{
-            if(expression.charAt(expression.length-1)!==''&&expression.charAt(expression.length-1)!==' '){
+            if(expression.charAt(expression.length-1)!==''&&expression.charAt(expression.length-1)!==' '){//If expression is empty or there is already an operator
                 expression+=` ${button.textContent} `;    
                 screen.textContent+=button.textContent;
             }
@@ -27,23 +35,34 @@ function calculate(){//An array length of 0 or an even array length are invalid
     }
     equation=expression.split(' ');
     result=Number(equation[0]);
+    let num;
     for(let i=1;i<equation.length;i+=2){
+        num=Number(equation[i+1]);
         if(equation[i]==='÷'){
-            result/=Number(equation[i+1]);
+            if(num!==0){
+                result/=num;    
+            }else{
+                result=0;
+            }
         }else if(equation[i]==='x'){
-            result*=Number(equation[i+1]);
+            result*=num;
         }else if(equation[i]==='-'){
-            result-=Number(equation[i+1]);
+            result-=num;
         }else if(equation[i]==='+'){
-            result+=Number(equation[i+1]);
+            result+=num;
         }
     }
     clear();
     screen.textContent=result;
+    result='';//For when somebody presses clear before inputting
 }
 
 function clear(){
     screen.textContent='';
-    expression='';
+    expression=''+result;
     equation=[];
+}
+
+function deleteThing(){
+
 }
